d292e157d5ab29dc4d92be3f1eb7101fa592128e
Loaf <168995949+1LoafaBread@users.noreply.github.com>
2024-07-01 12:14:50 -0400

    Add files via upload

---
+++ Visual Media Organizer/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Visual_Media_Organizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnNext = new Button();
+            btnSave = new Button();
+            btnBack = new Button();
+            btnSelectFolder = new Button();
+            pictureBox = new PictureBox();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            savedLabel = new Label();
+            btnClearCache = new Button();
+            lblFileName = new Label();
+            axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            lblFilesView = new Label();
+            btnKeybinds = new Button();
+            comboBoxOrgType = new ComboBox();
+            lblOrganizeType = new Label();
+            btnRefresh = new Button();
+            btnNextFolder = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnNext
+            // 
+            btnNext.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnNext.Enabled = false;
+            btnNext.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNext.Location = new Point(120, 652);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(48, 25);
+            btnNext.TabIndex = 4;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnSave.Enabled = false;
+            btnSave.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSave.Location = new Point(66, 652);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(48, 25);
+            btnSave.TabIndex = 5;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnBack
+            // 
+            btnBack.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnBack.Enabled = false;
+            btnBack.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBack.Location = new Point(12, 652);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(48, 25);
+            btnBack.TabIndex = 6;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            btnBack.Click += btnBack_Click;
+            // 
+            // btnSelectFolder
+            // 
+            btnSelectFolder.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSelectFolder.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSelectFolder.Location = new Point(1162, 652);
+            btnSelectFolder.Name = "btnSelectFolder";
+            btnSelectFolder.Size = new Size(90, 25);
+            btnSelectFolder.TabIndex = 7;
+            btnSelectFolder.Text = "Select Folder";
+            btnSelectFolder.UseVisualStyleBackColor = true;
+            btnSelectFolder.Click += btnSelectFolder_Click;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox.Location = new Point(40, 12);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(1180, 620);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 8;
+            pictureBox.TabStop = false;
+            // 
+            // savedLabel
+            // 
+            savedLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            savedLabel.AutoSize = true;
+            savedLabel.Font = new Font("Sitka Text", 11.249999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            savedLabel.Location = new Point(263, 652);
+            savedLabel.Name = "savedLabel";
+            savedLabel.Size = new Size(60, 21);
+            savedLabel.TabIndex = 9;
+            savedLabel.Text = "Saved!";
+            savedLabel.Visible = false;
+            // 
+            // btnClearCache
+            // 
+            btnClearCache.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnClearCache.Enabled = false;
+            btnClearCache.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClearCache.Location = new Point(1066, 652);
+            btnClearCache.Name = "btnClearCache";
+            btnClearCache.Size = new Size(90, 25);
+            btnClearCache.TabIndex = 10;
+            btnClearCache.Text = "Clear Cache";
+            btnClearCache.UseVisualStyleBackColor = true;
+            btnClearCache.Click += btnClearCache_Click;
+            // 
+            // lblFileName
+            // 
+            lblFileName.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lblFileName.AutoSize = true;
+            lblFileName.BackColor = Color.Transparent;
+            lblFileName.Font = new Font("Sitka Text", 9.749999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFileName.Location = new Point(9, 632);
+            lblFileName.Name = "lblFileName";
+            lblFileName.Size = new Size(65, 19);
+            lblFileName.TabIndex = 11;
+            lblFileName.Text = "fileName";
+            lblFileName.Visible = false;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            axWindowsMediaPlayer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            axWindowsMediaPlayer1.Enabled = true;
+            axWindowsMediaPlayer1.Location = new Point(40, 12);
+            axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            axWindowsMediaPlayer1.OcxState = (AxHost.State)resources.GetObject("axWindowsMediaPlayer1.OcxState");
+            axWindowsMediaPlayer1.Size = new Size(1180, 620);
+            axWindowsMediaPlayer1.TabIndex = 12;
+            axWindowsMediaPlayer1.Visible = false;
+            // 
+            // lblFilesView
+            // 
+            lblFilesView.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            lblFilesView.AutoSize = true;
+            lblFilesView.Font = new Font("Stencil", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFilesView.Location = new Point(1144, 634);
+            lblFilesView.Name = "lblFilesView";
+            lblFilesView.Size = new Size(32, 18);
+            lblFilesView.TabIndex = 13;
+            lblFilesView.Text = "N/A";
+            lblFilesView.Visible = false;
+            // 
+            // btnKeybinds
+            // 
+            btnKeybinds.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnKeybinds.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnKeybinds.Location = new Point(970, 652);
+            btnKeybinds.Name = "btnKeybinds";
+            btnKeybinds.Size = new Size(90, 25);
+            btnKeybinds.TabIndex = 14;
+            btnKeybinds.Text = "Keybinds";
+            btnKeybinds.UseVisualStyleBackColor = true;
+            btnKeybinds.Click += btnKeybinds_Click;
+            // 
+            // comboBoxOrgType
+            // 
+            comboBoxOrgType.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            comboBoxOrgType.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxOrgType.FormattingEnabled = true;
+            comboBoxOrgType.Items.AddRange(new object[] { "Date Modified (New->Old)", "Date Modified (Old->New)", "Name (A->Z)", "Name (Z->A)", "Random" });
+            comboBoxOrgType.Location = new Point(801, 654);
+            comboBoxOrgType.Name = "comboBoxOrgType";
+            comboBoxOrgType.Size = new Size(163, 23);
+            comboBoxOrgType.TabIndex = 15;
+            // 
+            // lblOrganizeType
+            // 
+            lblOrganizeType.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            lblOrganizeType.AutoSize = true;
+            lblOrganizeType.Location = new Point(841, 637);
+            lblOrganizeType.Name = "lblOrganizeType";
+            lblOrganizeType.Size = new Size(81, 15);
+            lblOrganizeType.TabIndex = 16;
+            lblOrganizeType.Text = "Organize Type";
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnRefresh.Enabled = false;
+            btnRefresh.Location = new Point(741, 654);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(54, 23);
+            btnRefresh.TabIndex = 17;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // btnNextFolder
+            // 
+            btnNextFolder.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnNextFolder.Enabled = false;
+            btnNextFolder.Font = new Font("Sitka Text", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNextFolder.Location = new Point(174, 652);
+            btnNextFolder.Name = "btnNextFolder";
+            btnNextFolder.Size = new Size(83, 25);
+            btnNextFolder.TabIndex = 19;
+            btnNextFolder.Text = "Next Folder";
+            btnNextFolder.UseVisualStyleBackColor = true;
+            btnNextFolder.Click += btnNextFolder_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(btnNextFolder);
+            Controls.Add(btnRefresh);
+            Controls.Add(lblOrganizeType);
+            Controls.Add(comboBoxOrgType);
+            Controls.Add(btnKeybinds);
+            Controls.Add(lblFilesView);
+            Controls.Add(axWindowsMediaPlayer1);
+            Controls.Add(lblFileName);
+            Controls.Add(btnClearCache);
+            Controls.Add(savedLabel);
+            Controls.Add(pictureBox);
+            Controls.Add(btnSelectFolder);
+            Controls.Add(btnBack);
+            Controls.Add(btnSave);
+            Controls.Add(btnNext);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MinimumSize = new Size(960, 540);
+            Name = "Form1";
+            Text = "Visual Media Organizer";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)axWindowsMediaPlayer1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnNext;
+        private Button btnSave;
+        private Button btnBack;
+        private Button btnSelectFolder;
+        private PictureBox pictureBox;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Label savedLabel;
+        private Button btnClearCache;
+        private Label lblFileName;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private Label lblFilesView;
+        private Button btnKeybinds;
+        private ComboBox comboBoxOrgType;
+        private Label lblOrganizeType;
+        private Button btnRefresh;
+        private Button btnNextFolder;
+    }
+}
---
+++ Visual Media Organizer/HotKeyForm.cs
@@ -0,0 +1,128 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_Media_Organizer
+{
+    public partial class HotKeyForm : Form
+    {
+        public Keys BackKey { get; private set; }
+        public Keys SaveKey { get; private set; }
+        public Keys NextKey { get; private set; }
+        public Keys NextFolderKey { get; private set; }
+
+        private bool listeningForKey = false;
+
+        public HotKeyForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += HotKeyForm_KeyDown;
+        }
+
+        private void btnBackChange_Click(object sender, EventArgs e)
+        {
+            listeningForKey = true;
+            lblStatus.Text = "Status: Listening";
+        }
+
+        private void btnSaveChange_Click(object sender, EventArgs e)
+        {
+            listeningForKey = true;
+            lblStatus.Text = "Status: Listening";
+        }
+
+        private void btnNextChange_Click(object sender, EventArgs e)
+        {
+            listeningForKey = true;
+            lblStatus.Text = "Status: Listening";
+        }
+
+        private void btnNextFolderChange_Click(object sender, EventArgs e)
+        {
+            listeningForKey = true;
+            lblStatus.Text = "Status: Listening";
+        }
+
+        public void LoadKeybinds()
+        {
+            string configFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Visual Media Organizer", "config.cfg");
+            if (File.Exists(configFilePath))
+            {
+                string json = File.ReadAllText(configFilePath);
+                var keybinds = JsonConvert.DeserializeObject<Keybinds>(json);
+                if (keybinds != null)
+                {
+                    BackKey = keybinds.BackKey;
+                    txtboxBackKeybind.Text = keybinds.BackKey.ToString();
+                    SaveKey = keybinds.SaveKey;
+                    txtboxSaveKeybind.Text = keybinds.SaveKey.ToString();
+                    NextKey = keybinds.NextKey;
+                    txtboxSaveKeybind.Text = keybinds.NextKey.ToString();
+                    NextFolderKey = keybinds.NextFolderKey;
+                    txtboxNextFolderKeybind.Text = keybinds.NextFolderKey.ToString();
+                }
+            }
+        }
+
+        private void SaveKeybinds()
+        {
+            string configFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Visual Media Organizer", "config.cfg");
+            var keybinds = new Keybinds
+            {
+                BackKey = BackKey,
+                SaveKey = SaveKey,
+                NextKey = NextKey,
+                NextFolderKey = NextFolderKey
+            };
+            string json = JsonConvert.SerializeObject(keybinds);
+            File.WriteAllText(configFilePath, json);
+        }
+
+        private void HotKeyForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listeningForKey)
+            {
+                if (btnBackChange.Focused)
+                {
+                    txtboxBackKeybind.Text = e.KeyCode.ToString();
+                    BackKey = e.KeyCode;
+                }
+                else if (btnSaveChange.Focused)
+                {
+                    txtboxSaveKeybind.Text = e.KeyCode.ToString();
+                    SaveKey = e.KeyCode;
+                }
+                else if (btnNextChange.Focused)
+                {
+                    txtboxNextKeybind.Text = e.KeyCode.ToString();
+                    NextKey = e.KeyCode;
+                }
+                else if (btnNextFolderChange.Focused)
+                {
+                    txtboxNextFolderKeybind.Text = e.KeyCode.ToString();
+                    NextFolderKey = e.KeyCode;
+                }
+
+                SaveKeybinds();
+                listeningForKey = false;
+                lblStatus.Text = "Status: Captured";
+            }
+        }
+    }
+
+    public class Keybinds
+    {
+        public Keys BackKey { get; set; }
+        public Keys SaveKey { get; set; }
+        public Keys NextKey { get; set; }
+        public Keys NextFolderKey { get; set; }
+    }
+}
